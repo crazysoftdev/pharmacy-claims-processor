3244c01bf187b944e2c126ee72625d3323b844e0
Joker <joker@Jokers-MacBook-Pro.local>
2025-07-03 06:43:57 -0500

    Initial commit: Spring Boot project skeleton from Initializer

---
+++ src/main/java/com/example/pharmacy_claims_processor/PharmacyClaimsProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacy_claims_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyClaimsProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyClaimsProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pharmacy_claims_processor/PharmacyClaimsProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharmacy_claims_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyClaimsProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
